cdac1e308989b2f87e5016c3b26bbb58e16bc4fd
Jesper Grooss <jgr@dhigroup.com>
2020-12-17 12:26:43 +0100

    User Guide - year updates.

--- CreateZip.bat
+++ CreateZip.bat
@@ -2,5 +2,5 @@ REM Make new Zip (non-recursively, do not add directory items, exclude the .MySC
 zip -D DHIMatlabToolbox.zip Documentation\*.pdf 
 zip -D DHIMatlabToolbox.zip Example\*.m Example\data\*.* 
 zip -D DHIMatlabToolbox.zip mbin\*.* mbin\@DFS\*.* mbin\Windows\*.*
-zip -D DHIMatlabToolbox.zip MatlabDfsUtil\*.* MatlabDfsUtil\MatlabDfsUtilBuildRel.bat 
+zip -D DHIMatlabToolbox.zip MatlabDfsUtil\*.*
 pause
